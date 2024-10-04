205b7853bc46ac5f011c4d93b0ec55c7b6911cce
SergeyShulgin <shulgen5000@mail.ru>
2024-10-04 23:18:54 +0300

    Add files via upload

---
+++ PP_2_1_2_Spring_HelloWorld-master/src/main/java/Cat.java
@@ -0,0 +1,10 @@
+public class Cat {
+    private String message;
+
+public void setMessage(String message){
+    this.message=message;
+    }
+    public String getMessege(){
+        return message;
+    }
+}
