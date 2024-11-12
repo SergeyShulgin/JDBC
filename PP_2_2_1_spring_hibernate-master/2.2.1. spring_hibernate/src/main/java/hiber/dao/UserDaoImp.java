bb210deed9167d663b7752d387170d81c1bc05c4
SergeyShulgin <shulgen5000@mail.ru>
2024-11-12 23:01:57 +0300

    Update UserDaoImp.java

--- PP_2_2_1_spring_hibernate-master/2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ PP_2_2_1_spring_hibernate-master/2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -27,11 +27,40 @@ public class UserDaoImp implements UserDao {
         return sessionFactory.getCurrentSession().createQuery("from User").getResultList();
     }
 
+package hiber.dao;
+
+import hiber.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void add(User user) {        
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        return sessionFactory.getCurrentSession().createQuery("from User").getResultList();
+    }
+
     public User getUserByCarModelAndSeries(String model, int series) {
-        Session session = sessionFactory.getCurrentSession();
-        Query<User> query = session.createQuery("FROM User u JOIN FETCH u.car c WHERE c.model = :model AND c.series = :series", User.class);
-        query.setParameter("model", model);
-        query.setParameter("series", series);
-        return query.uniqueResult();
+        return (User) sessionFactory.getCurrentSession()
+                .createQuery("FROM User u JOIN FETCH u.car c WHERE c.model = " + ":model AND c.series = :series", User.class)
+                .setParameter("model", model)
+                .setParameter("series", series)
+                .getResultList();
     }
 }
