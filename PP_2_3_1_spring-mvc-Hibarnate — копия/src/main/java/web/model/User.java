d824f92fe18263969e8b933d2d57b1001fcd5501
SergeyShulgin <shulgen5000@mail.ru>
2024-12-05 21:10:22 +0300

    Add files via upload

---
+++ PP_2_3_1_spring-mvc-Hibarnate — копия/src/main/java/web/model/User.java
@@ -0,0 +1,73 @@
+package web.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    public User(long id, int age, String name, String lastname) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", age=" + age +
+                ", name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
---
+++ PP_2_3_1_spring-mvc-Hibarnate — копия/src/main/java/web/service/UserService.java
@@ -0,0 +1,20 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> getAllUsers();
+
+    Object getUserById(long id);
+
+    void addUser(User user);
+
+    void removeUser(long id);
+
+    void updateUser(User user);
+
+}
